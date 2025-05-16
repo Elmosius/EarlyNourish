6dbe6967c7900d3861f6f9f1f90d61d5b237dbfc
Elmosius <2272008@maranatha.ac.id>
2025-05-16 15:00:19 +0700

    create skeleton pinia

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import { useCounterStore } from "../stores/counter.js";
+
+const counter = useCounterStore();
+</script>
 
 <template>
   <h1>ini home</h1>
+  <p>Counter: {{ counter.count }}</p>
 </template>
 
 <style scoped></style>
