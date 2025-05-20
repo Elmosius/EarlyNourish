6f5923c108b10f84594efc601efb101ffc397c16
Elmosius <2272008@maranatha.ac.id>
2025-05-20 11:25:33 +0700

    create footer component

---
+++ front-end/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <footer class="bg-white py-8 border-t border-gray-200 shadow-sm">
+    <div class="container mx-auto px-4 text-center">
+      <div class="flex justify-center items-center mb-4">
+        <img src="/logo.png" alt="Early Nourish" class="h-8 w-8" />
+        <span class="ml-2 font-semibold text-gray-800">Early Nourish</span>
+      </div>
+
+      <p class="text-gray-600 max-w-md mx-auto mb-6 text-base md:text-sm">
+        Mencegah stunting pada anak melalui deteksi dini dan nutrisi yang
+        dipersonalisasi.
+      </p>
+
+      <div class="w-45 h-px bg-gray-200 mx-auto mb-6"></div>
+
+      <p class="text-gray-500 text-[0.7rem] md:text-sm">
+        © 2025 Early Nourish. All rights reserved.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
