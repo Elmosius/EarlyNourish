ada0dece6faacc2c63ab7f7f4d817e9c5b72f1e0
Elmosius <2272008@maranatha.ac.id>
2025-06-02 13:42:14 +0700

    tambah cors

--- ml/main.py
+++ ml/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import predict 
 from core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI(
@@ -9,15 +10,28 @@ app = FastAPI(
     description="API untuk memprediksi risiko stunting pada anak dan memberikan rekomendasi."
 )
 
+origins = [
+    "http://localhost", # Jika FE dan BE sama-sama di localhost tapi beda port
+    "http://localhost:3000",  # Contoh jika FE React/Vue/Angular berjalan di port 3000
+    "http://localhost:5173",  # Contoh jika FE Vite (seperti Vue/React) berjalan di port 5173
+    # Tambahkan origin frontend kamu yang sebenarnya di sini nanti
+    # "https://domain-frontend-kamu.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True, 
+    allow_methods=["*"],    
+    allow_headers=["*"],    
+)
+
 app.include_router(predict.router)
 
 @app.get("/", tags=["Root"])
 async def read_root():
-    """
-    Endpoint root untuk salam sapa dan status API.
-    """
     return {
-        "message": "Selamat datang di API Deteksi Stunting Anak!",
+        "message": "Selamat datang di API ML!",
         "status": "Berjalan dengan baik",
         "docs": "/docs"
     }
