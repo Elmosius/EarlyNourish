120582278c25828b498cadf97be699ac30b304b1
Elmosius <2272008@maranatha.ac.id>
2025-06-02 19:44:24 +0700

    Create FormError.vue

---
+++ front-end/src/components/ui/FormError.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <p class="text-red-500 text-xs mt-1">{{ message }}</p>
+</template>
