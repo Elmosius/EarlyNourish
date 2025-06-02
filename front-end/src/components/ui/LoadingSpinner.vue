4b146b5e6802584d7d5cba86e6aeb7ac911b409a
Elmosius <2272008@maranatha.ac.id>
2025-06-02 18:01:30 +0700

    Create LoadingSpinner.vue

---
+++ front-end/src/components/ui/LoadingSpinner.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div
+      class="animate-spin rounded-full h-9 w-9 border-t-2 border-b-2 border-tertiary"
+    ></div>
+  </div>
+</template>
