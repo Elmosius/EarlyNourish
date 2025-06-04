c48e4fed5cacaa262b5594382f607b56fc170e17
Elmosius Suli <elmosius18@gmail.com>
2025-06-04 09:15:08 +0700

    Update AssessmentPage.vue

--- front-end/src/views/AssessmentPage.vue
+++ front-end/src/views/AssessmentPage.vue
@@ -1,6 +1,5 @@
 <script setup>
 import HeroSection from "../components/asesmen/HeroSection.vue";
-import FormSection from "../components/asesmen/FormSection.vue";
 import HelpSection from "../components/asesmen/HelpSection.vue";
 import InfoSection from "../components/asesmen/InfoSection.vue";
 import MultiFormSection from "../components/asesmen/MultiFormSection.vue";
