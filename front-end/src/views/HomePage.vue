503b7280c80a2b92545629d4a791076c3fe90fa3
Elmosius <2272008@maranatha.ac.id>
2025-06-05 20:23:54 +0700

    delete stores counter

--- front-end/src/views/HomePage.vue
+++ front-end/src/views/HomePage.vue
@@ -1,12 +1,9 @@
 <script setup>
-import { useCounterStore } from "../stores/counter.js";
 import HeroSection from "../components/home/HeroSection.vue";
 import AboutSection from "../components/home/AboutSection.vue";
 import FeatureSection from "../components/home/FeatureSection.vue";
 import CaraKerjaSection from "../components/home/CaraKerjaSection.vue";
 import TestimoniSection from "../components/home/TestimoniSection.vue";
-
-const counter = useCounterStore();
 </script>
 
 <template>
